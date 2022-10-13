3d4bec8cf05fd904b79a698f716d28d9c4cd5a41
Justin Lamb <justinlamb@me.com>
2022-10-13 11:43:45 -0700

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,26 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(user_params)
-    @user = User.new
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
     end
   end
 
